a2fe0302327f0ebe4a616484304b1a0232975b40
Kevin <purplefirepokemon@gmail.com>
2025-12-03 18:44:12 -0500

    should be ready to begin work on the lab report

--- ZhouK_Lab7B_main.m
+++ ZhouK_Lab7B_main.m
@@ -1,9 +1,11 @@
+% This file will run code once, plotting the graphs
+
 clc; clear all;
 
 XX = 68;
 YY = 84;
 
-w = 1;
+w = 0.7483;
 
 F = 10;
 m1 = 5;
@@ -19,10 +21,11 @@ f3 = @(vars) ((-1*(k1+k2)*vars(2))+(k2*vars(3))-((c1+c2)*vars(4))+(c2*vars(5)))/
 f4 = @(vars) ((F*cos(w*vars(1)))-(k2*vars(3))+(k2*vars(2))-(c2*vars(5))+(c2*vars(4)))/m2;
 funcs = {f1 f2 f3 f4};
 init = [0 0 0 0 0];
-h = 0.1;
+h = 0.01;
 eps = 0.0001;
-t = 30;
+t = 80;
 num_steps = t/h;
-ZhouK_Lab7B(funcs, init, h, num_steps, eps, true);
+xvals = ZhouK_Lab7B(funcs, init, h, num_steps, eps, true);
+ratio = xvals(2)/xvals(1)
 
 
